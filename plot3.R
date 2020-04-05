4e5ff83c29c24d9fe5908eab2e769cdbcfe5e2ff
Maria Garcia <ma.garcia1377@uniandes.edu.co>
2020-04-05 15:07:58 -0500

    Commit

--- plot3.R
+++ plot3.R
@@ -25,13 +25,14 @@ data <- cbind(data_datetime, data)
 data$data_datetime <- as.POSIXct(data_datetime)
 
 ##Ploting hist&saving 
+
 png("plot3.png", 480,480,"px")
 with(data, {
   plot(Sub_metering_1 ~data$data_datetime, type = "l", xlab = "", ylab = "Energy Sub metering")
   lines(Sub_metering_2 ~data$data_datetime, col = "red")
   lines(Sub_metering_3 ~data$data_datetime, col = "blue")
   legend("topright", col =c("black", "red", "blue"),  lwd = c(1,1,1) ,c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(data$Global_reactive_power~data$data_datetime, type = "l", xlab = "", ylab = "Global_reactive_power")
+  
 })
 
 dev.off()
